7cbfc082c2b08e96db3d7c85661a314b5eb34a1d
glowupmatt <glowupmatt@gmail.com>
2023-05-17 17:40:13 -0700

    Created Components/Styles, anc conected to API to link content to the Main page

---
+++ src/components/Videos.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Stack, Box } from "@mui/system";
+import { VideoCard, ChannelCard } from "./index";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, index) => {
+        return (
+          <Box key={index}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default Videos;
